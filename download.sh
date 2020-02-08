ebfd6520d73067bc07c6575967ea23958ad4acb5
jashanbhullar <jsbhullar96@gmail.com>
2020-02-08 15:25:45 +0530

    - bash script to download the m3u8 files

---
+++ download.sh
@@ -0,0 +1,14 @@
+ffmpeg \
+-i "https://eu2-cdn.nextstream.org/stream/0000/8fc8d1e6827a362a480bd2f787db7e05098b1c2c582bb2434cc11785c95cd33f/uwu.m3u8?token=gqrXqlylEScxS6zYDM7s9w&expires=1581126939" \
+headers - ":authority: eu2-cdn.nextstream.org" \
+headers - ":method: GET" \
+headers - ":path: /stream/0000/8fc8d1e6827a362a480bd2f787db7e05098b1c2c582bb2434cc11785c95cd33f/uwu.m3u8?token=FpJDm0q0dI5PU0zN_ZRk0A&expires=1581125761" \
+headers - ":scheme: https" \
+headers - "accept: */*" \
+headers - "accept-encoding: gzip, deflate, br" \
+headers - "accept-language: en-US,en;q=0.9" \
+headers - "origin: https://kwik.cx" \
+headers - "referer: https://kwik.cx/f/XLMiKAAhJSBc" \
+headers - "sec-fetch-mode: cors" \
+headers - "sec-fetch-site: cross-site" \
+headers - "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
