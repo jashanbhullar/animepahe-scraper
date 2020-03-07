0979d0c3677d3603f536a3ec209a4afea57e55f2
jashan <jsbhullar96@gmail.com>
2020-03-07 14:09:51 +0530

    - Custom script for donwload

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
-ffmpeg \
--i "https://eu2-cdn.nextstream.org/stream/0000/8fc8d1e6827a362a480bd2f787db7e05098b1c2c582bb2434cc11785c95cd33f/uwu.m3u8?token=gqrXqlylEScxS6zYDM7s9w&expires=1581126939" \
-headers - ":authority: eu2-cdn.nextstream.org" \
-headers - ":method: GET" \
-headers - ":path: /stream/0000/8fc8d1e6827a362a480bd2f787db7e05098b1c2c582bb2434cc11785c95cd33f/uwu.m3u8?token=FpJDm0q0dI5PU0zN_ZRk0A&expires=1581125761" \
-headers - ":scheme: https" \
-headers - "accept: */*" \
-headers - "accept-encoding: gzip, deflate, br" \
-headers - "accept-language: en-US,en;q=0.9" \
-headers - "origin: https://kwik.cx" \
-headers - "referer: https://kwik.cx/f/XLMiKAAhJSBc" \
-headers - "sec-fetch-mode: cors" \
-headers - "sec-fetch-site: cross-site" \
-headers - "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+# ffmpeg \
+# -headers "referer: https://kwik.cx/e/XLMiKAAhJSBc" \
+# -i "https://eu2-cdn.nextstream.org/stream/0000/8fc8d1e6827a362a480bd2f787db7e05098b1c2c582bb2434cc11785c95cd33f/uwu.m3u8?token=uf58U8xdtkXAHqYf1clwpA&expires=1583654491" \
+# -c copy -bsf:a aac_adtstoasc \
+# "Watch Boku no Hero Academia 4th Season - 64 Online.mp4"
+
+
+while read -r line; do
+    video=($line)
+    name=${video[0]}
+    url=${video[1]}
+    ffmpeg -headers "referer: https://kwik.cx/e/XLMiKAAhJSBc" -i "${url}" -c copy -bsf:a aac_adtstoasc "${name}"
+    break
+done < "$1"
